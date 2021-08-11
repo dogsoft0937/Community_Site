eb16c24a7319f3d0c5edd4d8339bfbf78c3a6443
dogsoft0937 <dogsoft0937@khu.ac.kr>
2021-08-11 19:06:20 +0900

    client & server initial

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express=require("express");
+const app=express();
+
+const port = process.env.PORT || 5000
+
+app.get("/",(req,res)=>{
+    res.send("helloworld");
+})
+
+
+app.listen(port,()=>{
+    console.log("Server start");
+})
+
