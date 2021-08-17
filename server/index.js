30e9e1f2e5c603f6b2c6e49098313b831c90bdaa
dogsoft0937 <dogsoft0937@khu.ac.kr>
2021-08-17 21:11:19 +0900

    server cors error solve

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 const express=require("express");
 const app=express();
 
+const cors=require("cors");
+
+
+//API 차단 문제 해결
+app.use(cors());
+
+
 const port = process.env.PORT || 5000
+
+
 
 app.get("/",(req,res)=>{
     res.send("test");
