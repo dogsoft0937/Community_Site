5410d054966f783e45577921ac414d0a273e84a9
dogsoft0937 <dogsoft0937@khu.ac.kr>
2021-08-18 16:40:25 +0900

    merge

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,12 +10,7 @@ function App() {
   return (
     <Suspense fallback={(<div>Loading...</div>)}>
       <NavBar />
-<<<<<<< HEAD
       <div style={{height:"70%"}}>
-=======
-      <div>네비게이션~</div>
-      <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
->>>>>>> 632805fa3da963a31fb3c9f3a7a017676d54662b
         <Switch>
           <Route exact path="/" component={LandingPage} />
           <Route exact path="/register" component={RegisterPage} />
