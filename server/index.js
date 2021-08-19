4194611fa4c8f625ae283bf6184964efe2cd964d
dogsoft0937 <dogsoft0937@khu.ac.kr>
2021-08-19 23:00:06 +0900

    backend login & register function develop

--- server/index.js
+++ server/index.js
@@ -2,12 +2,23 @@ const express=require("express");
 const app=express();
 
 
+//몽고디비 설정 불러오기
+const config =require('./config/key')
+
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
 const cors=require("cors");
 
 
+//디비 연결
+const mongoose = require('mongoose');
+const connect = mongoose.connect(config.mongoURI,{
+    useNewUrlParser: true, useUnifiedTopology: true,
+    useCreateIndex: true, useFindAndModify: false
+    })
+    .then(()=>console.log('디비연결 성공'))
+    .catch((err)=>console.log(err));
 
 
 
