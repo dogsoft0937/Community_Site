f8710a95d942b99ef7ac5047151016ccb8410794
이인서 <is5pm@khu.ac.kr>
2021-08-31 03:44:47 +0900

    Writepage

--- client/src/components/views/WritePage/WritePage.js
+++ client/src/components/views/WritePage/WritePage.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import axios from 'axios'
 
 function WritePage() {
     return (
-        <div>
-            글쓰기양식
-        </div>
+      <>
+      <div>
+        <input type='text' id = 'title_txt' name = 'title' placeholder = 'Title'/>
+      </div>
+
+      <div>
+        <textarea id='content_txt' name = 'contents' placeholder='내용을 입력하세요'> </textarea>
+      </div>
+
+      <div id = 'post_submit'>
+        <button> Submit </button>
+      </div>
+      </>
     )
 }
 
